f1e171a96d715fad19ff8c5ffcf47b064c1a1d98
Partho Das <parthodas6176@gmail.com>
2022-01-15 17:55:51 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import json
+import turtle
+import urllib.request
+import time
+
+screen = turtle.Screen()
+screen.setup(1200, 859)
+screen.setworldcoordinates(-180,-90,180,90)
+
+screen.bgpic("map.gif")
+screen.register_shape("iss.gif")
+iss = turtle.Turtle()
+iss.shape("iss.gif")
+#iss.setheading(45)
+iss.penup()
+
+while True:
+    url="http://api.open-notify.org/iss-now.json"
+    response = urllib.request.urlopen(url)
+    result = json.loads(response.read())
+
+    location = result["iss_position"]
+    lat = float(location['latitude'])
+    lon = float(location['longitude'])
+    
+    iss.goto(lon, lat)
+    if not iss.isdown():
+        iss.pendown()
+    #time.sleep(5)
