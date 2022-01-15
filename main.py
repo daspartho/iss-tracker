55ce888bbd0e79be345453ffa7c3c8188323bc61
Partho Das <parthodas6176@gmail.com>
2022-01-15 19:21:15 +0530

    organized into functions

--- main.py
+++ main.py
@@ -2,24 +2,29 @@ import json
 import turtle
 import urllib.request
 
-turtle.title('ISS Tracker')
-screen = turtle.Screen()
-screen.setup(1189, 848)
-screen.setworldcoordinates(-180,-90,180,90)
-
-screen.bgpic("map.gif")
-screen.register_shape("iss.gif")
-iss = turtle.Turtle()
-iss.shape("iss.gif")
-iss.penup()
-
-while True:
-    url="http://api.open-notify.org/iss-now.json"
-    response = urllib.request.urlopen(url)
-    result = json.loads(response.read())
-
+def get_coordinates():
+    response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
+    result = json.loads(response.read())   
     location = result["iss_position"]
     lat = float(location['latitude'])
     lon = float(location['longitude'])
+    return lon, lat
+
+def main():
+    turtle.title('ISS Tracker')
+    screen = turtle.Screen()
+    screen.setup(1189, 848)
+    screen.setworldcoordinates(-180,-90,180,90)
+    screen.bgpic("map.gif")   
+    screen.register_shape("iss.gif")
+    iss = turtle.Turtle()
+    iss.shape("iss.gif")
+    iss.penup()
     
-    iss.goto(lon, lat)
+    while True:
+        lon, lat = get_coordinates()
+        iss.goto(lon, lat)
+
+if __name__=='__main__':
+    main()
+
