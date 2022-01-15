7a763ff39e5cae1d1886c3a96c1986fde4c73859
Partho Das <parthodas6176@gmail.com>
2022-01-15 20:34:07 +0530

    added astronaut details

--- main.py
+++ main.py
@@ -2,15 +2,7 @@ import json
 import turtle
 import urllib.request
 
-def get_coordinates():
-    response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
-    result = json.loads(response.read())   
-    location = result["iss_position"]
-    lat = float(location['latitude'])
-    lon = float(location['longitude'])
-    return lon, lat
-
-def main():
+def setup():
     turtle.title('ISS Tracker')
     screen = turtle.Screen()
     screen.setup(1189, 848)
@@ -20,7 +12,30 @@ def main():
     iss = turtle.Turtle()
     iss.shape("iss.gif")
     iss.penup()
+    return iss
+
+def astronaut_details():
+    response = urllib.request.urlopen("http://api.open-notify.org/astros.json")
+    result = json.loads(response.read())
+    n= result["number"]
+    print(f"There are currently {n} astronauts on the ISS: ")
+    people = result["people"]
     
+    for p in people:
+        print(p['name'])
+
+def get_coordinates():
+    response = urllib.request.urlopen("http://api.open-notify.org/iss-now.json")
+    result = json.loads(response.read())   
+    location = result["iss_position"]
+    lat = float(location['latitude'])
+    lon = float(location['longitude'])
+    return lon, lat
+
+def main():
+    iss = setup()
+    astronaut_details()
+
     while True:
         lon, lat = get_coordinates()
         iss.goto(lon, lat)
